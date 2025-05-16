1fde20eaf6cd71959b9b22e9dd2ce87b4b2f6d31
Lilcsiway <lilcsiway@gmail.com>
2025-05-16 22:41:35 +0200

    download.js

--- download.js
+++ download.js
@@ -1,5 +1,5 @@
 var url = "https://i.pinimg.com/736x/b9/5b/d7/b95bd751e45d3f3da0837d509f5bce70.jpg";
-var file = "C:\\Users\\Public\\kep.jpg";
+var file = "C:\\Users\\feil\\Desktop\\PWS feladat\\kep.jpg";
 
 var xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 xmlhttp.Open("GET", url, false);
